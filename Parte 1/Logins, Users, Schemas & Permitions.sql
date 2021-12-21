7539a2a63b1acddfacc7debdd3a189097adbd418
JIBarrionuevoGaltier <juanimaximo30@live.com.ar>
2021-12-21 14:41:56 -0300

    Se agrega parte 1

---
+++ Parte 1/Logins, Users, Schemas & Permitions.sql
@@ -0,0 +1,42 @@
+--Create Login
+USE master
+GO
+CREATE LOGIN Academia WITH PASSWORD = '123456';
+
+--Crear los usuarios para el login
+USE jardineria
+GO
+CREATE USER Pepito FOR LOGIN Academia WITH DEFAULT_SCHEMA = Academia_Sch;
+GO
+
+--Asociar Esquema al usuario
+CREATE SCHEMA Academia_Sch AUTHORIZATION Pepito;
+GO
+
+--Dar permisos a Pepito sobre el esquema de dbo
+GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::dbo TO Pepito;
+
+--Otorgar Roles o Permisos a un usuario en específico
+GRANT CREATE TABLE TO Pepito;	--Pepito tiene permisos para crear tablas
+GO
+GRANT CREATE VIEW TO Pepito;	--Pepito tiene permisos para crear vistas
+GO
+
+--Otorgar todos los privilegios (mirar documentación)
+GRANT ALL PRIVILEGES TO Pepito;
+
+--Crear Roles
+CREATE ROLE profesor;	--Paso 1: Crear el rol
+GO
+GRANT CREATE TABLE TO profesor;	--Paso 2: Otorgar permisos al rol
+GO
+ALTER ROLE profesor ADD MEMBER Pepito;	--Paso 3: Incluir a Pepito dentro del grupo de roles del profesor
+GO
+--Borrar ROL
+EXECUTE sp_droprolemember profesor, Pepito; -- Borrar miembros
+DROP ROLE profesor;	--Extra: Se borra un rol (NO SE BORRA EL USUARIO, solo se quita el miembro)
+GO
+
+--Heredar ROLES
+ALTER ROLE db_datareader ADD MEMBER Ben;  
+GO
---
+++ Parte 1/README.md
@@ -0,0 +1,8 @@
+# T-SQL-Ejercicios
+Recolección de ejercicios para ser realizados en SQL Server.
+
+Los modelos fueron planteados en otras academias pero se cambian a los enunciados de los mismos para dar complejidad.
+Se agregan archivos a modo de ejemplo para la realización de algunos ejercicios.
+
+Año: 2021
+Autor: Juan Ignacio Barrionuevo Galtier
